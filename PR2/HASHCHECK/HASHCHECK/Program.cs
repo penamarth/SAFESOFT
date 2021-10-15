4f6c8851142d61d8879c85ee25962b07df52a238
penamarth <penamarth@gmail.com>
2021-10-15 23:02:10 +0300

    no message

---
+++ PR2/HASHCHECK/HASHCHECK/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+using System.Threading;
+
+namespace HASHCHECK
+{
+    class Hashcheck
+    {
+        static void Main(string[] args)
+        {
+            Thread myThread = new Thread(new ThreadStart(Count));
+            myThread.Start(); // запускаем поток
+
+            for (int i = 1; i < 9; i++)
+            {
+                Console.WriteLine("Главный поток:");
+                Console.WriteLine(i * i);
+                Thread.Sleep(300);
+            }
+
+
+            string plainData = "Mahesh";
+            Console.WriteLine("Raw data: {0}", plainData);
+            string hashedData = ComputeSha256Hash(plainData);
+            Console.WriteLine("Hash {0}", hashedData);
+            Console.WriteLine(ComputeSha256Hash("Mahesh"));
+            Console.ReadLine();
+        }
+
+        static string ComputeSha256Hash(string rawData)
+        {
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        public static void Count()
+        {
+            for (int i = 1; i < 9; i++)
+            {
+                Console.WriteLine("Второй поток:");
+                Console.WriteLine(i * i);
+                Thread.Sleep(400);
+            }
+        }
+    }
+}
